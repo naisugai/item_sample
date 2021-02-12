180cf0881c729052e9cbdd27468634e4e7ff22cf
須貝克仁 <>
2021-02-12 12:59:59 +0900

    no message

---
+++ db/migrate/20210212035932_create_images.rb
@@ -0,0 +1,10 @@
+class CreateImages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :images do |t|
+      t.references :item, null: false, foreign_key: true
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
